4b06d8840e9afff6608a1a43326704620238893a
yash <yaananth@microsoft.com>
2019-08-19 12:22:26 -0400

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 async function run() {
   try {
     const repo = core.getInput('repo');
-    core.debug(`Hello ${repo}`);
+    core.error(`Hello ${repo}`);
   } catch (error) {
     core.setFailed(error.message);
   }
