a6a98599bbb2151bda7b27f9e825a756048a3dc6
yash <yaananth@microsoft.com>
2019-08-19 14:21:09 -0400

    update

--- README.md
+++ README.md
@@ -2,6 +2,27 @@
 
 This github action upload a python project to pip using twine.
 
+# Contributing
+## Creating tag
+```
+git checkout -b releases/v1
+rm -rf node_modules
+sed -i '/node_modules/d' .gitignore # Bash command that removes node_modules from .gitignore
+git add node_modules .gitignore
+git commit -am node_modules
+git push origin releases/v1
+git push origin :refs/tags/v1
+git tag -fa v1 -m "Update v1 tag"
+git push origin v1
+```
+## Updating tag
+```
+npm run build
+git commit -am "update"
+git tag -fa v1 -m "Update v1 tag"
+git push origin v1 --force
+```
+
 ## Resources
 
 See the walkthrough located [here](https://github.com/actions/toolkit/blob/master/docs/javascript-action.md) and versioning [here](https://github.com/actions/toolkit/blob/master/docs/action-versioning.md).
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,59 @@
 import * as core from '@actions/core';
+import * as exec from'@actions/exec';
+import * as fs from "fs";
+import * as path from "path";
+
+
+const PypircName = "twineghubaction.pypirc";
+const getPypircPath = (root: string) => path.join(root, PypircName);
+
+interface IRunnerContext {
+  os: string;
+  tool_cache: string;
+  temp: string;
+  workspace: string;
+}
+
+interface ISecretsContext {
+  username: string;
+  password: string;
+}
 
 async function run() {
   try {
-    const repo = core.getInput('repo');
-    core.error(`Hello ${repo}`);
+    const runnerContext: IRunnerContext = JSON.parse(process.env.RUNNER_CONTEXT || "");
+    // Install dependencies
+    await exec.exec('python -m pip install --upgrade pip twine');
+    await exec.exec('pip install -r requirements.txt');
+
+    // Package
+    await exec.exec(`python setup.py sdist --dist-dir ${runnerContext.temp}`);  
+
+    // Create necessary config for twine
+    writePypirc(runnerContext);
+
+    // Upload
+    const configPath = getPypircPath(runnerContext.workspace);
+    await exec.exec(`twine upload -r nimport --config-file "${configPath}" ${runnerContext.temp}/* --skip-existing`);  
   } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+function writePypirc(runnerContext: IRunnerContext) {
+  const secretsContext: ISecretsContext = JSON.parse(process.env.SECRETS_CONTEXT || "");
+  const pypricContents = `
+  [distutils]
+    index-servers=
+      ${core.getInput('repo')}
+  [pypi]
+      username: ${secretsContext.username}
+      password: ${secretsContext.password}
+  `;
+  try {
+    fs.writeFileSync(getPypircPath(runnerContext.workspace), pypricContents);
+  }
+  catch(error) {
     core.setFailed(error.message);
   }
 }
